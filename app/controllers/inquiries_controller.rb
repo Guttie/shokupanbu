bd384167493d097c9769d525feb9f716e9bc9649
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-12-10 15:07:58 +0000

    [作成]TOP画面,説明画面,問い合わせ雛形

---
+++ app/controllers/inquiries_controller.rb
@@ -0,0 +1,13 @@
+class InquiriesController < ApplicationController
+  def new
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def done
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 問い合わせフォーム
+  resources :inquiries, only: [:new, :create]
+  post 'inquiries/confirm' => 'inquiries#confirm', as: 'confirm'
+  get 'done' => 'inquiries#done', as: 'done'
+  
+  root to: 'homes#top'
+  get 'about' => 'homes#about', as: 'about'
 end
