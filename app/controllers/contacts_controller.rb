b185c711e43384173b1462984301604dd0a2b917
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-12-12 06:23:59 +0000

    [作成]contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,43 @@
 class ContactsController < ApplicationController
+  
   def new
+    @contact = Contact.new
   end
 
+  # newアクションから入力内容を受け取り確認画面を生成
+  # 入力内容にエラーがあれば前の画面へ戻す
+  # sendボタンが押されたらcreateアクションを実行
   def confirm
+    @contact = Contact.new(contact_params)
+    if @contact.invalid?
+      render :new
+    end
+  end
+  
+  # 定義することで入力内容を保持したまま前のページに戻る
+  def back
+    @contact = Contact.new(contact_params)
+    render :new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContactMailer.send_mail(@contact).deliver_now
+      redirect_to done_path
+    else
+      render :new
+    end
   end
 
+  # 送信完了画面
   def done
   end
+  
+  private
+  
+  def contact_params
+    params.require(:contact).permit(:name, :email, :subject, :matter)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # 問い合わせフォーム
   resources :contacts, only: [:new, :create]
   post 'contacts/confirm' => 'contacts#confirm', as: 'confirm'
+  post 'contacts/back' => 'contacts#back', as: 'back'
   get 'done' => 'contacts#done', as: 'done'
   
   root to: 'homes#top'
