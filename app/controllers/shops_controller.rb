18ebcf9f2406eca2df6687251bde04e36a6945f9
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-12-18 05:38:10 +0000

    [追加]投稿編集機能

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -14,11 +14,30 @@ class ShopsController < ApplicationController
     @shop = Shop.new(shop_params)
     @shop.user_id = current_user.id
     @shop.save
-    redirect_to shops_path
+    redirect_to shop_path(@shop.id)
   end
 
   def show
     @shop = Shop.find(params[:id])
+  end
+  
+  def edit
+    @shop = Shop.find(params[:id])
+    if @shop.user_id = current_user.id
+      render :edit
+    else
+      redirect_to shops_path
+    end
+  end
+  
+  def update
+    @shop = Shop.find(params[:id])
+    @shop.user_id = current_user.id
+    if @shop.update(shop_params)
+      redirect_to shop_path(@shop.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
