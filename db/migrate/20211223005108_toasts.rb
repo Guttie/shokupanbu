ffef810d21ba091781170fa9b58a3ad9fc71955b
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-12-25 04:35:21 +0000

    [削除]マイグレーション関連

--- db/migrate/20211223005108_toasts.rb
+++
@@ -1,8 +0,0 @@
-class Toasts < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :toasts
-    drop_table :toast_images
-    drop_table :sandwiches
-    drop_table :sandwich_images
-  end
-end
