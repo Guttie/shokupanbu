ae14adf1a82629c7956d91ed59c84c80790cc307
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-12-25 04:31:19 +0000

    [削除]toastマイグレーション関連

--- db/migrate/20211220065829_change_cloumns_notnull_add_toasts.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCloumnsNotnullAddToasts < ActiveRecord::Migration[5.2]
-  def change
-    change_column :toasts, :name, :string, null: false
-    change_column :toasts, :review, :text, null: false
-    change_column :toasts, :user_id, :integer, null: false
-  end
-end
